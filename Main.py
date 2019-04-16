e71be7d9d4c065e94dc45cc359ed2c74cf5740d6
意大利跑 <451540776@qq.com>
2019-04-16 11:13:15 +0800

    完善框架

--- Main.py
+++ Main.py
@@ -10,12 +10,30 @@ app.config.from_object(config)
 def GetPage():
     return render_template('index.html')
 
-@app.route('/getdata',methods=['POST','GET'])
-def GetData():
+@app.route('/new_page',methods=['POST','GET'])
+def GetNewPage():
+    A = request.args.get("a")
+    B = request.args.get("b")
+    print("--------------GETNEWPAGE-------------")
+    print(A)
+    print(B)
+    return render_template('NewPage.html', data = {"result":"OK"})
+
+@app.route('/post_data',methods=['POST','GET'])
+def PostData():
     data = request.json
-    print('----')
+    print("-------------POST-----------")
     print(data)
-    return json.dumps(data)
+    return json.dumps({"result":"完成测试"})
+
+@app.route('/get_data',methods=['POST','GET'])
+def Data():
+    A = request.args.get("a")
+    B = request.args.get("b")
+    print("--------------GET-------------")
+    print(A)
+    print(B)
+    return json.dumps({"result":"完成测试"})
 
 if __name__ == '__main__':
     # app.run(host='192.168.1.103',port='8080',debug=True)
---
+++ static/js/index.js
@@ -0,0 +1,35 @@
+$("#bt_post").click(function () {
+	alert('OK');
+	$.ajax({
+		type: "post",
+		async: true, // 异步执行
+		url: "post_data",
+		data: JSON.stringify({'a':1}),
+		contentType: 'application/json',
+		dataType: "json", // 返回数据形式为json
+		success: function (json) {
+			console.log(json);
+			$("#dv_post").html("完成测试：" + json["result"])
+		},
+		error: function (errorMsg) {
+		}
+	});
+});
+
+$("#bt_get").click(function () {
+	alert('OK');
+	$.ajax({
+		type: "get",
+		async: true, // 异步执行
+		url: "get_data",
+		data: {"a":1, "b":2},
+		contentType: 'application/json',
+		dataType: "json", // 返回数据形式为json
+		success: function (json) {
+			console.log(json);
+			$("#dv_get").html("完成测试:" + json["result"]);
+		},
+		error: function (errorMsg) {
+		}
+    });
+});
