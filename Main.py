7be6ff4283deedbf384036d91e5e19c451d73f73
Ccatherineee <44626344+Ccatherineee@users.noreply.github.com>
2019-04-25 13:19:34 +0800

    数据库增删查改

--- Main.py
+++ Main.py
@@ -1,14 +1,20 @@
-from flask import Flask,render_template,url_for,redirect
-import flask
-import json
-from flask import request
-import config
+from flask import Flask, json, config,render_template, url_for, redirect, flash
+from flask import request,jsonify
+import DBsession
+from sqlalchemy import create_engine, and_, update, or_
+from model import *
+from Form import Login_Form, Register_Form
+from flask_login import LoginManager,login_user,UserMixin,login_required
+
 app = Flask(__name__)
 app.config.from_object(config)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mssql+pyodbc://sa:catherine@127.0.0.1:1433/data'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.secret_key = 'catherine'
 
-@app.route('/index',methods=['POST','GET'])
-def GetPage():
-    return render_template('index.html')
+# @app.route('/index',methods=['POST','GET'])
+# def GetPage():
+#     return render_template('index.html')
 
 @app.route('/new_page',methods=['POST','GET'])
 def GetNewPage():
@@ -17,7 +23,7 @@ def GetNewPage():
     print("--------------GETNEWPAGE-------------")
     print(A)
     print(B)
-    return render_template('NewPage.html', data = {"result":"OK"})
+    return render_template('NewPage.html', data={"result":"OK"})
 
 @app.route('/post_data',methods=['POST','GET'])
 def PostData():
@@ -34,6 +40,45 @@ def Data():
     print(A)
     print(B)
     return json.dumps({"result":"完成测试"})
+
+@app.route('/')
+def index():
+    form = Login_Form()
+    return render_template('login.html', form=form)
+
+@app.route('/login',methods=['GOT','POST'])
+def login():
+    form = Login_Form()
+    if form.validate_on_submit():
+        dbsession = DBsession.DatabaseManagement()
+        query_filter = and_(username = form.username.data)
+        user = dbsession.query_all(User, query_filter)
+        if user is not None and user.password==form.password.data:
+            login_user(user)
+            flash('登陆成功')
+            return render_template('')
+        else:
+            flash('用户名或密码错误')
+            return render_template('login.html',form = form)
+
+@app.route('/register',methods=['GOT','POST'])
+def register():
+    form = Register_Form()
+    if request.method == 'Get':
+        return render_template('register.html',form = form)
+    elif request.method == 'POST':
+        if form.validate_on_submit():
+            if form.password.data == form.repassword.data:
+                user = User(username=form.username.data, password=form.password.data)
+                dbsession = DBsession.DatabaseManagement()
+                dbsession.add_obj(user)
+                flash(message = '注册成功')
+                return redirect('/login')
+            else:
+                flash(message='两次输入密码不一致，请重新输入！')
+                return render_template('/register.html',form=form)
+        else:
+            return 'None'
 
 if __name__ == '__main__':
     # app.run(host='192.168.1.103',port='8080',debug=True)
---
+++ model.py
@@ -0,0 +1,142 @@
+# coding: utf-8
+from sqlalchemy import Column, DECIMAL, Float, Integer, NCHAR, String, Table
+from sqlalchemy.ext.declarative import declarative_base
+from flask_login import UserMixin
+Base = declarative_base()
+metadata = Base.metadata
+
+
+class DictClas(Base):
+    __tablename__ = 'Dict_Class'
+
+    Code = Column(String(50, 'Chinese_PRC_CI_AS'))
+    Class = Column(String(100, 'Chinese_PRC_CI_AS'))
+    id = Column(Integer, primary_key=True)
+    Subsys = Column(String(50, 'Chinese_PRC_CI_AS'))
+
+
+class ExpertJudge(Base):
+    __tablename__ = 'Expert_Judge'
+
+    ID = Column(Integer, primary_key=True)
+    Class = Column(String(100, 'Chinese_PRC_CI_AS'))
+    Year = Column(String(50, 'Chinese_PRC_CI_AS'))
+    Expert = Column(String(50, 'Chinese_PRC_CI_AS'))
+    Stage = Column(Integer)
+
+
+class ExpertList(Base):
+    __tablename__ = 'Expert_List'
+
+    Id = Column(Integer, primary_key=True)
+    Expert_Name = Column(String(100, 'Chinese_PRC_CI_AS'))
+    Birth_Year = Column(String(50, 'Chinese_PRC_CI_AS'))
+    Sex = Column(String(50, 'Chinese_PRC_CI_AS'))
+    People = Column(String(50, 'Chinese_PRC_CI_AS'))
+    Institution = Column(String(50, 'Chinese_PRC_CI_AS'))
+    Professional_Title = Column(String(50, 'Chinese_PRC_CI_AS'))
+    Administrative_duty = Column(String(500, 'Chinese_PRC_CI_AS'))
+    City = Column(String(50, 'Chinese_PRC_CI_AS'))
+    Province = Column(String(50, 'Chinese_PRC_CI_AS'))
+    Address = Column(String(50, 'Chinese_PRC_CI_AS'))
+    Zip = Column(String(50, 'Chinese_PRC_CI_AS'))
+    Highestdegree = Column(String(50, 'Chinese_PRC_CI_AS'))
+    Degreedate = Column(String(50, 'Chinese_PRC_CI_AS'))
+    University = Column(String(50, 'Chinese_PRC_CI_AS'))
+    Country = Column(String(50, 'Chinese_PRC_CI_AS'))
+    Honorary_Reward = Column(String(500, 'Chinese_PRC_CI_AS'))
+    Tel = Column(String(50, 'Chinese_PRC_CI_AS'))
+    Email = Column(String(50, 'Chinese_PRC_CI_AS'))
+    Class = Column(String(500, 'Chinese_PRC_CI_AS'))
+
+
+t_Paper_Country_HotDegree = Table(
+    'Paper_Country_HotDegree', metadata,
+    Column('class', String(100, 'Chinese_PRC_CI_AS')),
+    Column('year', String(50, 'Chinese_PRC_CI_AS')),
+    Column('country', String(50, 'Chinese_PRC_CI_AS')),
+    Column('Hot_degree', DECIMAL(18, 3))
+)
+
+
+t_Paper_Country_Influence = Table(
+    'Paper_Country_Influence', metadata,
+    Column('class', String(100, 'Chinese_PRC_CI_AS')),
+    Column('year', String(50, 'Chinese_PRC_CI_AS')),
+    Column('Influence', Float(53)),
+    Column('country', String(50, 'Chinese_PRC_CI_AS'))
+)
+
+
+t_Paper_HotDegree = Table(
+    'Paper_HotDegree', metadata,
+    Column('class', String(100, 'Chinese_PRC_CI_AS')),
+    Column('year', String(50, 'Chinese_PRC_CI_AS')),
+    Column('Paper_Num_total', Integer),
+    Column('Hot_degree', DECIMAL(18, 3))
+)
+
+
+t_Paper_Influence = Table(
+    'Paper_Influence', metadata,
+    Column('class', String(100, 'Chinese_PRC_CI_AS')),
+    Column('year', String(50, 'Chinese_PRC_CI_AS')),
+    Column('Influence', Float(53)),
+    Column('subsys', NCHAR(10))
+)
+
+
+t_Patent_Country_HotDegree = Table(
+    'Patent_Country_HotDegree', metadata,
+    Column('class', String(100, 'Chinese_PRC_CI_AS')),
+    Column('year', String(50, 'Chinese_PRC_CI_AS')),
+    Column('country', String(50, 'Chinese_PRC_CI_AS')),
+    Column('Hot_degree', DECIMAL(18, 3))
+)
+
+
+t_Patent_Country_Influence = Table(
+    'Patent_Country_Influence', metadata,
+    Column('class', String(100, 'Chinese_PRC_CI_AS')),
+    Column('year', String(50, 'Chinese_PRC_CI_AS')),
+    Column('Patent_fc_class_country', Float(53)),
+    Column('country', String(50, 'Chinese_PRC_CI_AS'))
+)
+
+
+t_Patent_HotDegree = Table(
+    'Patent_HotDegree', metadata,
+    Column('class', String(100, 'Chinese_PRC_CI_AS')),
+    Column('year', String(50, 'Chinese_PRC_CI_AS')),
+    Column('Hot_degree', DECIMAL(18, 3))
+)
+
+
+t_Patent_Influence = Table(
+    'Patent_Influence', metadata,
+    Column('class', String(100, 'Chinese_PRC_CI_AS')),
+    Column('year', String(50, 'Chinese_PRC_CI_AS')),
+    Column('Influence', Float(53)),
+    Column('subsys', NCHAR(10))
+)
+
+
+class User(UserMixin, Base):
+    __tablename__ = 'User'
+
+    id = Column(Integer, primary_key=True)
+    username = Column(String(50, 'Chinese_PRC_CI_AS'), unique=True)
+    email = Column(String(50, 'Chinese_PRC_CI_AS'), unique=True)
+    password = Column(String(50, 'Chinese_PRC_CI_AS'))
+    def __init__(self,username, password, email):
+        self.username = username
+        self.password = password
+        self.email = email
+    def get_id(self):
+        return unicode(self.id)
+    
+class Country(Base):
+    __tablename__ = 'country'
+
+    ID = Column(Integer, primary_key=True)
+    Country = Column(String(50, 'Chinese_PRC_CI_AS'))
